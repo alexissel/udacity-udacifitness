e4348b3348dbaed8ad14160a0521bf872e7ff018
Alexis <alexis@intelcoder.com>
2019-04-17 08:46:53 +0300

    Styling in React Native

--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,15 +1,29 @@
 import React from 'react'
-import { View, Text, Slider } from 'react-native'
+import { View, Text, Slider, StyleSheet } from 'react-native'
+import { gray } from '../utils/colors'
 
 export default function UdaciSlider ({ max, unit, step, value, onChange }) {
   return (
-    <View>
-      <Slider step={step} value={value} minimumValue={0} maximumValue={max} onValueChange={onChange} />
+    <View style={styles.row}>
+      <Slider step={step} value={value} minimumValue={0} maximumValue={max} onValueChange={onChange} style={{flex: 1}} />
 
-      <View>
-      	<Text>{value}</Text>
-      	<Text>{unit}</Text>
+      <View style={styles.metricCounter}>
+        <Text style={{fontSize: 24, textAlign: 'center'}}>{value}</Text>
+        <Text style={{fontSize: 18, color: gray}}>{unit}</Text>
       </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    // flex: 1,
+    alignItems: 'center',
+  },
+  metricCounter: {
+    width: 85,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
